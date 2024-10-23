1935020136a245acb8e7682b1e8945347f80df55
MUHAMMAD NOUMAN MAHMOD <noumanmalik1919@gmail.com>
2024-10-23 15:16:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// function change(){
+//     const randomcolor = "#" + Math.floor(Math.random()*16777215).toString(16);
+//     document.body.style.backgroundColor = randomcolor;
+// }
+
+
+
+
+function change(){
+    const randomcolor = "#" +  Math.floor(Math.random()*16777).toString(16);
+    document.body.style.backgroundColor = randomcolor;
+}
